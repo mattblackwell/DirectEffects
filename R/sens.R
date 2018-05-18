0a3ccfdae027541590383a7e007d7ffc8a6e1104
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-05-17 23:31:03 -0400

    draft that gives output, with reliance on seq.g.var() and manual entry of medvar and trvar

--- R/sens.R
+++ R/sens.R
@@ -1,29 +1,51 @@
-pan.lag <- function(x, ind, lag = 1) {
-  unlist(tapply(x,ind, function(x) c(rep(NA, times = lag),x[-((length(x) - lag +1):length(x))])))
+seq.g.var <- function(mod.first, mod.direct, med.vars) {
+  require(sandwich)
+  mat.x <- model.matrix(mod.direct)
+  mat.first <- model.matrix(mod.first)
+  n <- nrow(mat.x)
+  Fhat <- crossprod(mat.x, mat.first)/n
+  Fhat[, !(colnames(mat.first) %in% med.vars)] <- 0
+  Mhat.inv <- solve(crossprod(mat.first)/n)
+  ghat <- t(estfun(mod.direct)) + Fhat %*% Mhat.inv %*% t(estfun(mod.first))
+  meat <- crossprod(t(ghat))/n
+  bread <- (t(mat.x)%*%mat.x)/n
+  vv <- (n/(n-ncol(mat.x)))*(solve(bread) %*% meat %*% solve(bread))/n
+  return(vv)
 }
 
-
-cdesens <- function(y_mod, m_mod, rho =  seq(-0.9,0.9, by = 0.05), data) {
+cdesens <- function(seqg, trvar = "plow", medvar = "centered_ln_inc", rho =  seq(-0.9,0.9, by = 0.05)) {
+  data = seqg$model
   
   acde.sens <- rep(NA, times = length(rho))
   acde.sens.se <- rep(NA, times = length(rho))
-  res.y <- residuals(lm(onset ~ pan.lag(warl, ccode, 1) +  pan.lag(gdpenl, ccode, 1) + pan.lag(lpop, ccode, 2) + pan.lag(polity2l, ccode, 1) + lmtnest + ncontig + Oil  + ethfrac + relfrac, data = data, subset = !is.na(instab2)))
-  res.m <- residuals(lm(instab2 ~  pan.lag(warl, ccode, 1) +  pan.lag(gdpenl, ccode, 1) + pan.lag(lpop, ccode, 2) + pan.lag(polity2l, ccode, 1) + lmtnest + ncontig + Oil  + ethfrac + relfrac, data = data))
-  rho.tilde <- cor(res.y, res.m)
-  m.fixed <- coef(first)["instab2"] - rho*sd(res.y)*sqrt((1-rho.tilde^2)/(1-rho^2))/sd(res.m)
-  n <- nrow(model.matrix(direct))
+  n <- nrow(seqg$model)
+  form.ytilde <- as.formula(paste0("y.tilde ~ ", paste0(names(coefficients(seqg))[-1], collapse = " + "))) # Ytilde ~ A + X
+  form.m <- as.formula(paste0(medvar, " ~ ", paste0(names(coefficients(seqg))[-1], collapse = " + "))) # M ~ A + X
   
+  res.y <- residuals(seqg$first_mod)
+  res.m <- residuals(lm(form.m, data))
+  
+  rho.tilde <- cor(res.y, res.m) # scalar
+  
+  # for each value of rho, change mediator value
+  m.fixed <- coef(seqg$first_mod)[medvar] - rho*sd(res.y)*sqrt((1-rho.tilde^2)/(1-rho^2))/sd(res.m)
+  
+  # calculate acde at each rho
   for (i in 1:length(rho)) {
-    sens.direct  <- lm(I(onset - m.fixed[i]*instab2) ~ lmtnest + ncontig  + Oil  + ethfrac + relfrac, data = data, subset = rownames(data) %in% rownames(model.matrix(first)))
-    acde.sens[i] <- coef(sens.direct)["ethfrac"]
-    sens.vcov <- sequential_g(first, sens.direct, "instab2")
-    acde.sens.se[i] <- sqrt(sens.vcov["ethfrac", "ethfrac"])
+    
+    y.tilde <- seqg$model[[1]] - m.fixed[i]*(seqg$model[[medvar]])
+    mf.i <- cbind(seqg$model, y.tilde)
+    
+    sens.direct  <- lm(form.ytilde, data = mf.i) # Ytilde ~ A + X
+    acde.sens[i] <- coef(sens.direct)[trvar]
+    
+    sens.vcov <- seq.g.var(seqg$first_mod, sens.direct, med.vars = medvar)
+    acde.sens.se[i] <- sqrt(sens.vcov[trvar, trvar])
   }
   
   list(
     direct = sens.direct,
     acde = acde.sens,
-    se = acde.sens.se,
-    vcov = sens.vcov,
+    se = acde.sens.se
   )
 }
