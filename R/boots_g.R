bede2bf0191590d964768304984881de44221e8d
Jacob Brown <jacobbrown@Jacobs-MacBook-Air-3.local>
2018-06-11 09:37:26 -0400

    Annotated bootstrap function and ran roxygenize() to create documentation.

--- R/boots_g.R
+++ R/boots_g.R
@@ -45,33 +45,42 @@
 
 boots_g = function(seqg, boots = 1000){
   require(R.utils)
-  acde.boots <- ate.boots <- pt.boots <- rep(list(NA), times = boots)
-  pb <- txtProgressBar(min = 0, max = boots, style = 3)
+  acde.boots <- ate.boots <- pt.boots <- rep(list(NA), times = boots) # holder for boot strap estimates
+  pb <- txtProgressBar(min = 0, max = boots, style = 3) #start progress bar
   for (b in 1:boots) {
     
-    setTxtProgressBar(pb, b)
-    draw = sample(1:nrow(seqg$model), replace = T)
-    data.draw.first <- seqg$first_mod$model[draw,]
-    data.draw.direct <- seqg$model[draw,]
-    shared <- colnames(data.draw.direct)[colnames(data.draw.direct) %in% colnames(data.draw.first)]
+    setTxtProgressBar(pb, b) # update progress bar
+    
+    #bootstrap sampling
+    draw = sample(1:nrow(seqg$model), replace = T) #vector for sample with replacement
+    data.draw.first <- seqg$first_mod$model[draw,] # data for re-estimation of first model
+    data.draw.direct <- seqg$model[draw,] # data for re-estimation of direct effects model
+    
+    #combine data
+    shared <- colnames(data.draw.direct)[colnames(data.draw.direct) %in% colnames(data.draw.first)] 
     just.direct <- colnames(data.draw.direct)[!(colnames(data.draw.direct) %in% colnames(data.draw.first))]
     just.first <- colnames(data.draw.first)[!(colnames(data.draw.first) %in% colnames(data.draw.direct))]
     data.draw <- cbind(data.draw.direct[,shared],
                       data.draw.direct[,just.direct],
                        data.draw.first[,just.first])
     colnames(data.draw) = c(shared, just.direct, just.first)
-    ate.mod <- lm(formula(seqg$formula, lhs = 1, rhs = 1), data = data.draw)
-    boot.first <- lm(seqg$first_mod$call[[2]], data = data.draw)
-    boot.direct <- sequential_g(seqg$formula, boot.first, data = data.draw)    
-    acde.boots[[b]] <- coef(boot.direct)
-    ate.boots[[b]] <- coef(ate.mod)
-    pt.boots[[b]] <- coef(boot.first)
+    
+    #estimate models
+    ate.mod <- lm(formula(seqg$formula, lhs = 1, rhs = 1), data = data.draw) #estimate ATE
+    boot.first <- lm(seqg$first_mod$call[[2]], data = data.draw) #estimate first model
+    boot.direct <- sequential_g(seqg$formula, boot.first, data = data.draw)    # estimate direct effects model
+    acde.boots[[b]] <- coef(boot.direct) # store direct effect coefficients
+    ate.boots[[b]] <- coef(ate.mod) # store ate coefficients
+    pt.boots[[b]] <- coef(boot.first) # store first model coefficients
   }
   close(pb)
-  acde.boots = do.call(rbind, acde.boots)
-  ate.boots = do.call(rbind, ate.boots)
-  pt.boots = do.call(rbind, pt.boots)
   
+  #combine lists into matrices
+  acde.boots = do.call(rbind, acde.boots) 
+  ate.boots = do.call(rbind, ate.boots)
+  pt.boots = do.call(rbind, pt.boots) 
+  
+  #construct output
   out=list()
   out$acde.sd = sapply(as.data.frame(acde.boots), sd)
   out$ate.sd = sapply(as.data.frame(ate.boots), sd)
