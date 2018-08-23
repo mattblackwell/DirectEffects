ba1feb41bde995a65f55fedf6cdf4f7c0403c6fe
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-08-23 18:25:07 -0400

    move boots_g to separate file, overwrite older one, make it not exported

--- R/boots_g.R
+++ R/boots_g.R
@@ -1,58 +1,26 @@
-#' Bootstrap Standard Errors for Sequential G model object
+
+#' Bootstrap standard errors
 #'
-#' Estimate bootstrap standard errors
+#' Called internally in sequential_g
 #'
 #' @param seqg Output from sequential_g
 #' @param boots The number of bootstrap replicates. Defaults to 1000.
 #' @param progress Show a textbar for progress?
 #'
 #'
-#' @export
-#' @return
-#' \item{acde.sd}{a vector of bootstrapped standard errors for each named coefficient for the direct effects model.}
-#' \item{ate.sd}{a vector of bootstrapped stanadard errors for each named coefficient for the
-#' average treatment effects effects model.}
-#' \item{pt.sd}{a vector of bootstrapped stanadard errors for each named coefficient for the
-#' first stage effects model.}
-#'
-#'
-#' @examples
-#' data(civilwar)
-#'
-#' # First stage
-#' fit_first <- lm(onset ~ ethfrac + warl + gdpenl + lpop +
-#'                   ncontig + Oil + nwstate + polity2l + relfrac + instab,
-#'                 data = civilwar)
-#'
-#'
-#' # main formula: Y ~ A + X | M
-#' form_main <- onset ~ ethfrac + lmtnest + ncontig + Oil | instab
-#'
-#' # estimate CDE
-#' direct <- sequential_g(form_main, fit_first, data = civilwar)
-#'
-#' # bootstrap
-#' out_boot <- boots_g(direct)
-#'
-#' # print bootstrapped standard errors
-#' print(out_boot)
-#'
-
-
+#' @keywords internal
 boots_g <- function(seqg, boots = 1000, progress = TRUE) {
-  
   acde.boots <- ate.boots <- pt.boots <- rep(list(NA), times = boots) # holder for boot strap estimates
-  
   if (progress) prog.bar <- utils::txtProgressBar(min = 0, max = boots, style = 3) # start progress bar
   
   for (b in 1:boots) {
     if (progress) utils::setTxtProgressBar(prog.bar, b) # update progress bar
-
+    
     # bootstrap sampling
     draw <- sample(1:nrow(seqg$model), replace = TRUE) # vector for sample with replacement
     data.draw.first <- seqg$first_mod$model[draw, ] # data for re-estimation of first model
     data.draw.direct <- seqg$model[draw, ] # data for re-estimation of direct effects model
-
+    
     # combine data
     shared <- colnames(data.draw.direct)[colnames(data.draw.direct) %in% colnames(data.draw.first)]
     just.direct <- colnames(data.draw.direct)[!(colnames(data.draw.direct) %in% colnames(data.draw.first))]
@@ -63,7 +31,7 @@ boots_g <- function(seqg, boots = 1000, progress = TRUE) {
       data.draw.first[, just.first]
     )
     colnames(data.draw) <- c(shared, just.direct, just.first)
-
+    
     # estimate models
     ate.mod <- lm(formula(seqg$formula, lhs = 1, rhs = 1), data = data.draw) # estimate ATE
     ate.boots[[b]] <- coef(ate.mod) # store ate coefficients
@@ -75,19 +43,19 @@ boots_g <- function(seqg, boots = 1000, progress = TRUE) {
     acde.boots[[b]] <- coef(boot.direct) # store direct effect coefficients
   }
   
-  if (progress)  close(prog.bar)
-
+  if (progress) close(prog.bar)
+  
   # combine lists into matrices
-  ate.boots  <- do.call(rbind, ate.boots)
+  ate.boots <- do.call(rbind, ate.boots)
   acde.boots <- do.call(rbind, acde.boots)
-  pt.boots   <- do.call(rbind, pt.boots)
-
+  pt.boots <- do.call(rbind, pt.boots)
+  
   # construct output
   out <- list()
-  out$acde.sd  <- sapply(as.data.frame(acde.boots), sd)
-  out$ate.sd   <- sapply(as.data.frame(ate.boots), sd)
-  out$pt.sd    <- sapply(as.data.frame(pt.boots), sd)
-
-
+  out$acde.sd <- sapply(as.data.frame(acde.boots), sd)
+  out$ate.sd <- sapply(as.data.frame(ate.boots), sd)
+  out$pt.sd <- sapply(as.data.frame(pt.boots), sd)
+  
+  
   return(out)
 }
