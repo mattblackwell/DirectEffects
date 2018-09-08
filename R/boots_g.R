5c20edcd10d062844bb395060c6227b87b2ff3b1
Jacob Brown <jrbrown@g.harvard.edu>
2018-09-08 18:33:50 -0400

    Added rho argument and ported over code from cdesens to do the blipping down by rhos as in the current bootstrap code in the cdesens() function. Now we need to alter the cdesens() code to integrate the new boots_g() functionality.

--- R/boots_g.R
+++ R/boots_g.R
@@ -7,13 +7,19 @@
 #' @param boots The number of bootstrap replicates. Defaults to 1000.
 #' @param progress Show a textbar for progress. Defaults to TRUE.
 #' @param data Dataframe to be used if none provided in seqg.
+#' @param rho Vector to be used if called from cdesens().
 #'
 #'
 #' @keywords internal
 
 
-boots_g <- function(seqg, boots = 1000, progress = TRUE, data = NULL) {
-  acde.boots <- rep(list(NA), times = boots) # holder for boot strap estimates
+boots_g <- function(seqg, boots = 1000, progress = TRUE, data = NULL, rho = NULL) {
+  # if called from cdesens, create acde.sens instead of acde.boots object
+  if(!is.null(rho)){
+    acde.sens <- matrix(NA, nrow = boots, ncol = length(rho)) # bootstrap samples as rows
+  } else{
+    acde.boots <- rep(list(NA), times = boots) # holder for boot strap estimates
+  }
   if (progress) prog.bar <- utils::txtProgressBar(min = 0, max = boots, style = 3) # start progress bar
   ## must be valid formula
   formula <- Formula::as.Formula(seqg$formula)
@@ -36,6 +42,39 @@ boots_g <- function(seqg, boots = 1000, progress = TRUE, data = NULL) {
 
     X <- model.matrix(seqg$terms$ax, data = data.draw, seqg$contrasts)
     M <- model.matrix(seqg$terms$blip, data = data.draw, seqg$contrasts)
+    if(!is.null(rho)){
+
+      # residuals
+      # epsilon.tilde.i.m: residuals of mediation function
+      res.m <- residuals(lm.fit(x = X, y = M))
+      
+      # epsilon.tilde.i.y: all variables in first model except medvar
+      form.first.y.X <- update(seqg$first_mod$terms, paste0(". ~ . -", medvar))
+      res.y <- residuals(lm(form.first.y.X, data = data.draw))
+      
+      rho.tilde <- cor(res.y, res.m)
+      
+      # for each value of the vector rho, change mediator value
+      rho.factor <- rho * sqrt((1 - rho.tilde^2) / (1 - rho^2))
+      m.fixed <- coef(first_mod)[medvar] - sd(res.y) * rho.factor / sd(res.m)
+      
+      # calculate acde at each rho (indexed by r)
+      for (r in 1:length(rho)) {
+        
+        # create ytilde by blipping down with rho
+        Ytilde <- model.response(data.draw, "numeric") - m.fixed[r] * (data.draw[[medvar]])
+        mf.r <- cbind(Ytilde, data.draw)
+        
+        # run Ytilde ~ A + X
+        sens.direct.r <- lm(form.Ytilde, data = mf.r)
+        
+        # save final estimate
+        acde.sens[b, r] <- coef(sens.direct.r)[trvar]
+      } # close rho loop
+    } else {
+    
+    
+    
     rawY <- model.response(data.draw, "numeric")
     gamma <- M %*% fcoefs[bvars] # fitted values from de-mediation function
     Y <- rawY - gamma
@@ -56,13 +95,19 @@ boots_g <- function(seqg, boots = 1000, progress = TRUE, data = NULL) {
 
     #####
 
-    # boot.direct <- sequential_g(seqg$formula, first_mod = boot.first, data = data.draw) # estimate direct effects model
     acde.boots[[b]] <- coef(boot.direct) # store direct effect coefficients
+    }
   }
 
   if (progress) close(prog.bar)
 
-  # combine list into matrix
+  # if called from cdesens, only output acde.sens
+  if(!is.null(rho)){
+    out <- acde.sens
+    return(out)
+  } else {
+    # combine list into matrix
+    
   acde.boots <- do.call(rbind, acde.boots)
 
   # construct output
@@ -72,4 +117,6 @@ boots_g <- function(seqg, boots = 1000, progress = TRUE, data = NULL) {
 
 
   return(out)
-}
+  } 
+  }
+
