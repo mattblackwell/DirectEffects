c172403fc8b8b5fe026d69c3b77fe4066a40c90d
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2018-10-25 23:03:29 -0400

    example

--- R/boots_g.R
+++ R/boots_g.R
@@ -6,6 +6,20 @@
 #' @param seqg Output from sequential_g
 #' @param boots The number of bootstrap replicates. Defaults to 1000.
 #' 
+#' @examples 
+#' 
+#' data(ploughs)
+#' form <- women_politics ~ plow +
+#'  agricultural_suitability + tropical_climate + large_animals + rugged | 
+#'  years_civil_conflict + years_interstate_conflict  + oil_pc +
+#'  european_descent + communist_dummy + polity2_2000 | 
+#'  centered_ln_inc + centered_ln_incsq
+#' s1 <- sequential_g(form, ploughs)
+#'  
+#' out.boots <- boots_g(s1)
+#' 
+#' out.boots
+#' 
 #' @export
 #'
 boots_g <- function(seqg, boots = 1000) {
