841e6edccf02ee25a14f853ade6ada10a9da3df1
Matthew Blackwell <mblackwell@gov.harvard.edu>
2018-12-18 23:27:59 -0500

    updated vignette for all-in-one

--- R/DirectEffects.R
+++ R/DirectEffects.R
@@ -82,19 +82,10 @@
 #' @examples
 #' data(ploughs)
 #'
-#' fit_first <- lm(
-#'   women_politics ~ plow + centered_ln_inc + centered_ln_incsq +
-#'     agricultural_suitability + tropical_climate +  large_animals +
-#'     political_hierarchies + economic_complexity +
-#'     rugged + years_civil_conflict +
-#'     years_interstate_conflict  + oil_pc +
-#'     european_descent + communist_dummy + polity2_2000 +
-#'     serv_va_gdp2000, data = ploughs)
-#'
 #' form_main <- women_politics ~ plow +
 #'   agricultural_suitability + tropical_climate + large_animals +
 #'   political_hierarchies + economic_complexity +
-#'   rugged | + years_civil_conflict +
+#'   rugged | years_civil_conflict +
 #'   years_interstate_conflict  + oil_pc +
 #'   european_descent + communist_dummy + polity2_2000 +
 #'   serv_va_gdp2000 | centered_ln_inc +centered_ln_incsq
