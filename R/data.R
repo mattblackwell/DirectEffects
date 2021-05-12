ec0806994b955a94f72948bd55d297eece4dc9a5
Matthew Blackwell <mblackwell@gov.harvard.edu>
2021-05-12 14:22:27 -0400

    url fixes, R 3.5.0

--- R/data.R
+++ R/data.R
@@ -38,7 +38,7 @@
 #' @name ploughs
 #' @usage data(ploughs)
 #' @format A data frame with 234 observations and 57 variables.
-#' @source \url{http://qje.oxfordjournals.org/content/128/2/469}
+#' @source \doi{10.1093/qje/qjt005}
 #' @references Alesina, A., Giuliano, P., & Nunn, N. (2013). On the
 #' Origins of Gender Roles: Women and the Plough. The Quarterly
 #' Journal of Economics, 128(2), 469-530.
@@ -126,7 +126,7 @@ NULL
 #' @name civilwar
 #' @usage data(civilwar)
 #' @format A data frame with 6610 observations and 69 variables.
-#' @source \url{https://doi.org/10.1017/S0003055403000534}
+#' @source \doi{10.1017/S0003055403000534}
 #' @references Fearon, James D., and David A. Laitin (2003). Ethnicity, Insurgency, and Civil War.
 #' American Political Science Review, 97(1), 75-90. doi:10.1017/S0003055403000534
 NULL
